ae31377b9dc2727eceea044403b6685577fa977f
Mark Wright <markscottwright@gmail.com>
2024-12-05 08:25:04 -0500

    Day 5

---
+++ src/main/java/adventofcode2024/Day5.java
@@ -0,0 +1,94 @@
+package adventofcode2024;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.collections4.multimap.HashSetValuedHashMap;
+
+public class Day5 {
+
+	ArrayList<List<Integer>> pageUpdates = new ArrayList<>();
+	HashSetValuedHashMap<Integer, Integer> pageToAfters = new HashSetValuedHashMap<Integer, Integer>();
+
+	public static Day5 parse(String input) {
+		Day5 day5 = new Day5();
+
+		boolean inOrderingRules = true;
+		for (String line : input.split("\n")) {
+			if (line.isBlank())
+				inOrderingRules = false;
+			else if (inOrderingRules) {
+				String[] beforeAndAfter = line.split("\\|");
+				int before = Integer.parseInt(beforeAndAfter[0]);
+				int after = Integer.parseInt(beforeAndAfter[1]);
+				day5.pageToAfters.put(before, after);
+			} else {
+				day5.pageUpdates.add(Arrays.asList(line.split(",")).stream().map(Integer::parseInt).toList());
+			}
+		}
+
+		return day5;
+	}
+
+	public boolean updateIsGood(List<Integer> update) {
+		for (int i = 0; i < update.size(); i++) {
+			Integer page = update.get(i);
+			Set<Integer> afters = pageToAfters.get(page);
+			
+			// all pages after this one appear in this page's after set
+			for (int j = i + 1; j < update.size(); ++j) {
+				Integer shouldBeAfter = update.get(j);
+				if (!afters.contains(shouldBeAfter)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public static Integer middlePage(List<Integer> update) {
+		assert update.size() % 2 == 1;
+		return update.get(update.size() / 2);
+	}
+
+	public int sumOfGoodMiddlePages() {
+		return pageUpdates.stream().filter(u -> updateIsGood(u)).mapToInt(Day5::middlePage).sum();
+	}
+
+	public int sumOfBadMiddlePages() {
+		return pageUpdates.stream().filter(u -> !updateIsGood(u)).map(this::reorder).mapToInt(Day5::middlePage).sum();
+	}
+
+	private List<Integer> reorder(List<Integer> list) {
+		ArrayList<Integer> out = new ArrayList<Integer>(list);
+		// we can use our list of after pages as a Comparator::compare and sort the list
+		out.sort(new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				if (o1.equals(o2))
+					return 0;
+
+				else if (pageToAfters.get(o1).contains(o2))
+					return -1;
+				else
+					return 1;
+			}
+		});
+
+		return out;
+	}
+
+	public static void main(String[] args) throws IOException {
+		Day5 day5 = Day5.parse(Files.readString(Path.of("data/day5.dat")));
+		System.out.println("Day 5 part 1: " + day5.sumOfGoodMiddlePages());
+		System.out.println("Day 5 part 2: " + day5.sumOfBadMiddlePages());
+	}
+
+}
--- src/main/java/adventofcode2024/Point.java
+++ src/main/java/adventofcode2024/Point.java
@@ -25,6 +25,7 @@ public class Point {
 	public String toString() {
 		return "Point [x=" + x + ", y=" + y + "]";
 	}
+
 	private int x;
 	private int y;
 
@@ -32,22 +33,24 @@ public class Point {
 		this.x = x;
 		this.y = y;
 	}
-	
-	Point n() {return new Point(x, y-1);}
+
+	//@formatter:off
+	Point n()  {return new Point(x, y-1);}
 	Point ne() {return new Point(x+1, y-1);}
-	Point e() {return new Point(x+1, y);}
+	Point e()  {return new Point(x+1, y);}
 	Point se() {return new Point(x+1, y+1);}
-	Point s() {return new Point(x, y+1);}
+	Point s()  {return new Point(x, y+1);}
 	Point sw() {return new Point(x-1, y+1);}
-	Point w() {return new Point(x-1, y);}
+	Point w()  {return new Point(x-1, y);}
 	Point nw() {return new Point(x-1, y-1);}
 
-	static Point toN(Point p) {return p.n();}
+	static Point toN(Point p)  {return p.n();}
 	static Point toNe(Point p) {return p.ne();}
-	static Point toE(Point p) {return p.e();}
+	static Point toE(Point p)  {return p.e();}
 	static Point toSe(Point p) {return p.se();}
-	static Point toS(Point p) {return p.s();}
+	static Point toS(Point p)  {return p.s();}
 	static Point toSw(Point p) {return p.sw();}
-	static Point toW(Point p) {return p.w();}
+	static Point toW(Point p)  {return p.w();}
 	static Point toNw(Point p) {return p.nw();}
+	//@formatter:on
 }
