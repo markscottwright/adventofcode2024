2aa4b615df10fd2b43bdbab055fd88e0046e0822
Mark Wright <markscottwright@gmail.com>
2024-12-05 12:04:58 -0500

    Add All days.

---
+++ src/main/java/adventofcode2024/All.java
@@ -0,0 +1,11 @@
+package adventofcode2024;
+
+public class All {
+	public static void main(String[] args) throws Exception {
+		Day1.main(args);
+		Day2.main(args);
+		Day3.main(args);
+		Day4.main(args);
+		Day5.main(args);
+	}
+}
